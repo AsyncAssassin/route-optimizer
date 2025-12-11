1bcc14365ca281786d91901e2c4d7eaae26dc69a
AsyncAssassin <iusupov.aa@phystech.edu>
2025-12-12 00:22:28 +0400

    Реализован решатель с выбором компромисса и форматированный вывод

---
+++ src/solver/RouteSolver.java
@@ -0,0 +1,135 @@
+package solver;
+
+import graph.DijkstraPathFinder;
+import graph.Graph;
+import model.City;
+import model.Criterion;
+import model.Route;
+import parser.InputParser.Request;
+
+import java.util.*;
+
+/**
+ * Решатель задачи оптимизации маршрутов.
+ * Находит оптимальные маршруты по каждому критерию и выбирает компромиссный вариант.
+ */
+public class RouteSolver {
+
+    private final Graph graph;
+    private final DijkstraPathFinder pathFinder;
+
+    public RouteSolver(Graph graph) {
+        this.graph = graph;
+        this.pathFinder = new DijkstraPathFinder(graph);
+    }
+
+    /**
+     * Результат решения одного запроса.
+     * Содержит три оптимальных маршрута и один компромиссный.
+     */
+    public static class SolutionResult {
+        private final Request request;
+        private final Map<Criterion, Route> optimalRoutes;
+        private final Route compromiseRoute;
+
+        public SolutionResult(Request request, Map<Criterion, Route> optimalRoutes, Route compromiseRoute) {
+            this.request = request;
+            this.optimalRoutes = optimalRoutes;
+            this.compromiseRoute = compromiseRoute;
+        }
+
+        public Request getRequest() {
+            return request;
+        }
+
+        public Map<Criterion, Route> getOptimalRoutes() {
+            return optimalRoutes;
+        }
+
+        public Route getCompromiseRoute() {
+            return compromiseRoute;
+        }
+    }
+
+    /**
+     * Решает запрос на построение маршрута.
+     * 
+     * @param request запрос с городами отправления/назначения и приоритетами
+     * @return результат с оптимальными и компромиссным маршрутами
+     */
+    public SolutionResult solve(Request request) {
+        City from = graph.getCityByName(request.getFromCity());
+        City to = graph.getCityByName(request.getToCity());
+
+        // Находим оптимальные маршруты по всем критериям
+        Map<Criterion, Route> optimalRoutes = pathFinder.findAllOptimalPaths(from, to);
+
+        // Выбираем компромиссный маршрут на основе приоритетов
+        Route compromiseRoute = selectCompromise(optimalRoutes, request.getPriorities());
+
+        return new SolutionResult(request, optimalRoutes, compromiseRoute);
+    }
+
+    /**
+     * Выбирает компромиссный маршрут на основе заданных приоритетов.
+     * 
+     * Логика выбора:
+     * 1. Собираем все уникальные маршруты из трёх оптимальных.
+     * 2. Сортируем их по приоритетам: сначала по первому критерию,
+     *    при равенстве — по второму, затем по третьему.
+     * 3. Возвращаем лучший маршрут.
+     * 
+     * @param optimalRoutes маршруты, оптимальные по каждому критерию
+     * @param priorities    список критериев в порядке убывания важности
+     * @return компромиссный маршрут
+     */
+    private Route selectCompromise(Map<Criterion, Route> optimalRoutes, List<Criterion> priorities) {
+        // Собираем уникальные маршруты
+        Set<Route> uniqueRoutes = new HashSet<>(optimalRoutes.values());
+
+        // Если все маршруты одинаковые — возвращаем любой
+        if (uniqueRoutes.size() == 1) {
+            return uniqueRoutes.iterator().next();
+        }
+
+        // Сортируем по приоритетам
+        List<Route> sortedRoutes = new ArrayList<>(uniqueRoutes);
+        sortedRoutes.sort((r1, r2) -> compareByPriorities(r1, r2, priorities));
+
+        return sortedRoutes.get(0);
+    }
+
+    /**
+     * Сравнивает два маршрута по списку приоритетов.
+     * 
+     * @param r1         первый маршрут
+     * @param r2         второй маршрут
+     * @param priorities критерии в порядке убывания важности
+     * @return отрицательное число если r1 лучше, положительное если r2 лучше, 0 если равны
+     */
+    private int compareByPriorities(Route r1, Route r2, List<Criterion> priorities) {
+        for (Criterion criterion : priorities) {
+            int value1 = r1.getValueByCriterion(criterion);
+            int value2 = r2.getValueByCriterion(criterion);
+
+            if (value1 != value2) {
+                return Integer.compare(value1, value2); // Меньше = лучше
+            }
+        }
+        return 0; // Полностью равные маршруты
+    }
+
+    /**
+     * Решает список запросов.
+     * 
+     * @param requests список запросов
+     * @return список результатов
+     */
+    public List<SolutionResult> solveAll(List<Request> requests) {
+        List<SolutionResult> results = new ArrayList<>();
+        for (Request request : requests) {
+            results.add(solve(request));
+        }
+        return results;
+    }
+}
---
+++ src/writer/OutputWriter.java
@@ -0,0 +1,74 @@
+package writer;
+
+import model.Criterion;
+import model.Route;
+import solver.RouteSolver.SolutionResult;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Записывает результаты решения в выходной файл.
+ * Формат вывода соответствует требованиям задания.
+ */
+public class OutputWriter {
+
+    /**
+     * Записывает все результаты в файл.
+     * 
+     * @param results  список результатов решения
+     * @param filename путь к выходному файлу
+     * @throws IOException при ошибке записи
+     */
+    public void write(List<SolutionResult> results, String filename) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (int i = 0; i < results.size(); i++) {
+                writeResult(writer, results.get(i));
+
+                // Добавляем пустую строку между запросами (кроме последнего)
+                if (i < results.size() - 1) {
+                    writer.newLine();
+                }
+            }
+        }
+    }
+
+    /**
+     * Записывает результат одного запроса.
+     */
+    private void writeResult(BufferedWriter writer, SolutionResult result) throws IOException {
+        // Порядок вывода критериев: ДЛИНА, ВРЕМЯ, СТОИМОСТЬ
+        Criterion[] outputOrder = {Criterion.DISTANCE, Criterion.TIME, Criterion.COST};
+
+        // Выводим оптимальные маршруты по каждому критерию
+        for (Criterion criterion : outputOrder) {
+            Route route = result.getOptimalRoutes().get(criterion);
+            writeLine(writer, criterion.getFullName(), route);
+        }
+
+        // Выводим компромиссный маршрут
+        writeLine(writer, "КОМПРОМИСС", result.getCompromiseRoute());
+    }
+
+    /**
+     * Записывает одну строку результата.
+     * Формат: КРИТЕРИЙ: Город1 -> Город2 -> ... | Д=..., В=..., С=...
+     */
+    private void writeLine(BufferedWriter writer, String label, Route route) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(label).append(": ");
+
+        if (route.exists()) {
+            sb.append(route.getPathString());
+            sb.append(" | ");
+            sb.append(route.getParamsString());
+        } else {
+            sb.append("Маршрут не найден");
+        }
+
+        writer.write(sb.toString());
+        writer.newLine();
+    }
+}
